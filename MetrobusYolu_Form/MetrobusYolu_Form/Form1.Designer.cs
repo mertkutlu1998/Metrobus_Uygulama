bbf33e8dc496db4f4950d3ca169cf080656e65dd
Mert KUTLU <114591763+mertkutlu1998@users.noreply.github.com>
2022-10-17 21:04:19 +0300

    Metrobus_uygulaması

---
+++ MetrobusYolu_Form/MetrobusYolu_Form/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace MetrobusYolu_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(284, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PENDİK";
+
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(558, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "KARTAL";
+
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(789, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "MALTEPE";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(30, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(1155, 33);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "----->";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(35, 141);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(145, 148);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(54, 313);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 32);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(1044, 42);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "KADIKÖY";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(1180, 313);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 32);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(32, 45);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 16);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "ZAMAN";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(1196, 46);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 16);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "ZAMAN";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1284, 357);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Name = "Form1";
+            this.Text = "METROBÜS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ MetrobusYolu_Form/MetrobusYolu_Form/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MetrobusYolu_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int zaman = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer2.Start();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            zaman++;
+            label6.Text = zaman.ToString();
+
+            Random random = new Random();
+            int x = random.Next(255);
+            int y = random.Next(255);
+            int z = random.Next(255);
+            if (zaman < 25)
+            {
+                pictureBox1.Left += 8;
+                label4.Text = "-" + label4.Text;
+            }
+            if (zaman >= 25 && zaman <= 45)
+            {
+                label1.BackColor = Color.FromArgb(x, y, z);
+                if (zaman == 45)
+                {
+                    label1.BackColor = Color.DarkGray;
+                }
+            }
+            if (zaman >=45 && zaman <= 80)
+            {
+                pictureBox1.Left += 8;
+                label4.Text = "-" + label4.Text;
+            }
+            if (zaman >= 80 && zaman <= 105)
+            {
+                label2.BackColor = Color.FromArgb(x, y, z);
+                if (zaman == 105)
+                {
+                    label2.BackColor = Color.DarkGray;
+                }
+            }
+            if (zaman >= 105 && zaman <= 135)
+            {
+                pictureBox1.Left += 8;
+                label4.Text = "-" + label4.Text;
+            }
+            if (zaman >= 135 && zaman <= 155)
+            {
+                label3.BackColor = Color.FromArgb(x, y, z);
+                if (zaman == 155)
+                {
+                    label3.BackColor = Color.DarkGray;
+                }
+            }
+            if (zaman >= 155 && zaman <= 185)
+            {
+                pictureBox1.Left += 8;
+                label4.Text = "-" + label4.Text;
+            }
+            if (zaman >= 185 && zaman <= 215)
+            {
+                label5.BackColor = Color.FromArgb(x, y, z);
+                if (zaman == 215)
+                {
+                    label5.BackColor = Color.DarkGray;
+                    timer1.Stop();
+                }
+            }
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            zaman--;
+            label7.Text = zaman.ToString();
+            Random random = new Random();
+            int x = random.Next(255);
+            int y = random.Next(255);
+            int z = random.Next(255);
+            if (zaman <= 215 && zaman >= 185)
+            {
+                pictureBox1.Left -= 8;
+            }
+            if (zaman <= 185 && zaman >= 145)
+            {
+                label3.BackColor = Color.FromArgb(x, y, z);
+                if (zaman == 145)
+                {
+                    label3.BackColor = Color.DarkGray;
+                }
+            }
+            if (zaman <= 145 && zaman >= 115)
+            {
+                pictureBox1.Left -= 8;
+            }
+            if (zaman <= 115 && zaman >= 75)
+            {
+                label2.BackColor = Color.FromArgb(x, y, z);
+                if (zaman == 75)
+                {
+                    label2.BackColor = Color.DarkGray;
+                }
+            }
+
+            if (zaman <= 75 && zaman >= 42)
+            {
+                pictureBox1.Left -= 8;
+
+            }
+            if (zaman <= 42 && zaman >= 25)
+            {
+                label1.BackColor = Color.FromArgb(x, y, z);
+                if (zaman == 45)
+                {
+                    label1.BackColor = Color.DarkGray;
+                }
+            }
+            if (zaman <= 25 && zaman >= 0)
+            {
+                pictureBox1.Left -=8;
+            }
+            if (zaman == 0)
+            {
+                timer2.Stop();
+            }
+        }
+    }
+}
